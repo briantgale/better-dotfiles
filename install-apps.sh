8570372b7c5d58dd5af3a3b00bd71308ef4cab41
Briant Gale <briantgale@gmail.com>
2019-02-17 22:31:22 -0700

    Moved initial install stuff out of linking shell script

---
+++ install-apps.sh
@@ -0,0 +1,18 @@
+echo -n "Install all base packages (Y/n)?"; read answer
+if [[ $answer != "n" ]] && [[ $answer != "N" ]] ; then
+  sudo chown -R $(whoami):admin /usr/local
+ 	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+ 
+ 	brew doctor
+ 	brew update
+ 
+ 	brew install neovim
+ 	brew install tmux
+ 	brew install git
+ 	brew install redis
+ 	brew install terminal-notifier
+ 
+ 	gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+ 	\curl -sSL https://get.rvm.io | bash -s stable
+fi
+
